9fba5a39b20b580c85157363376501a4e0ea70be
Susana Moreno <susana.moreno@hola.com>
2023-09-21 11:53:39 +0200

    [ED2-3716] - Redirections static content

--- apache-redirects/latest/reload.sh
+++ apache-redirects/latest/reload.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-
 docker stop apache
 docker rm apache
 docker build -t holadevdocker/apache-redirects:latest -f Dockerfile .
 docker run -d -p 80:80 --name apache holadevdocker/apache-redirects
-
-#while read d; do echo "RedirectMatch 301 "$d; done <redirs > .htaccess
